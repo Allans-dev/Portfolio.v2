4095fd9897679ab5baf07552869bf1437f7a9b93
Allan.W.C <allan_195@hotmail.com>
2017-05-07 15:59:19 +1000

    query change and contact form
    timeline is now visible to tablets

--- contact.php
+++ contact.php
@@ -3,3 +3,35 @@
 session_start();
 
 require_once 'libs/phpmailer/PHPMailerAutoload.php';
+
+$fields = [
+    'name' => $_POST['name'],
+    'email' => $_POST['email'],
+    'query-type' => $_POST['query-type'],
+    'message' => $_POST['message']
+]
+
+$m = new PHPMAiler;
+
+$m->isSMTP();
+$m->SMTPAuth = true;
+
+
+
+$m->Host = 'smtp.gmail.com';
+$m->Username = 'allanswebdev@gmail.com';
+$m->Password = 'Poker76537';
+$m->SMTPSecure = 'ssl';
+$m->Port = 465;
+
+$m->isHTML();
+
+$m->Subject = 'Contact Form Submitted' . $fields["query-type"];
+$m->Body = 'From:' . $fields["name"] . '(' . $fields["email"] . ') <p>' . $fields["message"] . '</p>';
+$m->FromName = 'Portfolio';
+$m->AddAddress('allanswebdev@gmail.com', 'Allan Cheung');
+
+if ($m->send()) {
+    
+    
+}
