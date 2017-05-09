f9f0c2c41e36043587814f5efc88193c4ab9e293
Allan.W.C <allan_195@hotmail.com>
2017-05-09 11:40:18 +1000

    dl and send work now
    fixed php issues

--- send.php
+++ send.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $queryType = $_POST['query-type'];
@@ -7,10 +11,10 @@ $message = $_POST['message'];
 
 $to = "allanswebdev@gmail.com";
 $subject = "$email: $queryType";
-$body = "From: $name ($email) \n\n $message"
+$body = "From: $name ($email) \n\n $message";
 
-mail ($to, $subject, $body)    
+mail ($to, $subject, $body);    
     
-echo "Thanks for your message! \n <a href='www.allancheung.com'>click here to go back</a> "
+echo "Thanks for your message! \n <a href='/'>click here to go back</a>";
     
 ?>
